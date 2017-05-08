f84f14b2a66ab47b8ab32dcc3bbc6c474cc9126a
rebecca palmore <rebeccapalmore@Rebeccas-MacBook-Pro.local>
2017-05-08 09:38:59 -0500

    updating

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,19 @@
--- In the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
--- Create the burgers_db.
--- Switch to or use the burgers_db.
+-- In the db folder, create a file named schema.sql. Write SQL queries IN this file that do the following:
+
+-- Create the burgers_db. X
+-- Switch to or use the burgers_db. X
 -- Create a burgers table with these fields:
--- id: an auto incrementing int that serves as the primary key.
--- burger_name: a string.
--- devoured: a boolean.
--- date: a TIMESTAMP.
+-- id: an auto incrementing int that serves as the primary key. X
+-- burger_name: a string. X
+-- devoured: a boolean. X
+-- date: a TIMESTAMP. X
+
+CREATE DATABASE burgers_db;
+
+CREATE TABLE burgers_db.burgers (
+	id int NOT NULL AUTO_INCREMENT,
+	burger_name varchar(300) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	date TIMESTAMP,
+	PRIMARY KEY (id)
+);
--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,27 @@
-// Inside burger.js, import orm.js into burger.js
-// Also inside burger.js, create the code that will call the ORM functions using burger specific input for the ORM.
-// Export at the end of the burger.js file.
+// Create the code that will call the ORM functions using burger specific input for the ORM.
+
+// Import the ORM 
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  create: function(cols, vals, cb) {
+    orm.create("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for burgers_controller.js 
+module.exports =  burger;
+
