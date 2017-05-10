cee30a9e34a7f9ccaaf48f6c244c9dbc3398b862
rebecca palmore <rebeccapalmore@Rebeccas-MacBook-Pro.local>
2017-05-10 08:25:40 -0500

    updating jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,7 @@ var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL); } else {
-var connection = mysql.createConnection({
-  port: 3306,
+connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
